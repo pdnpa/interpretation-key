993aaff8e0662d2fe1d8c1644bfddfd9585468bd
David Alexander <dga.1984@gmail.com>
2022-11-08 08:56:31 +0000

    Changes to sidebar

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -49,19 +49,83 @@ module.exports = {
     sidebar: {
       '/Definitions/': [
         {
-          title: 'Wood and forest land',
+          title: 'C Wood and forest land',
           collapsable: false,
           children: [
-            '',
-            'using-vue',
+            'C1 Broadleaved high forest',
+            'C2 Coniferous high forest ',
+            'C3 Mixed high forest'
+            'C4 Scrub',
+            'C5 Clear felled/new plantings in forest areas'
           ]
         },
         {
-          title: 'Grassland',
+          title: 'D Moor and Heathland',
           collapsable: false,
           children: [
-            '',
-            'using-vue',
+            'D1 Upland heath',
+            'D2 Upland grass moor (b) grass moor',
+            'D2 Upland grass moor (b) blanket peat grass moor',
+            'D3 Bracken',
+            'D4 Unenclosed lowland areas (a) rough grassland',
+            'D4 Unenclosed lowland areas (b) heath',
+            'D6 Upland mosaics (a) heath/grass',
+            'D6 Upland mosaics (b) heath/bracken',
+            'D6 Upland mosaics (c) heath/blanket peat',
+            'D7 Eroded area (a) peat',
+            'D7 Eroded area (b) mineral soils',
+            'D8 Coastal heath'
+          ]
+        },
+        {
+          title: 'E Agro-pastoral land',
+          collapsable: false,
+          children: [
+            'E1 Cultivated land',
+            'E2 Grassland (a) improved pasture',
+            'E2 Grassland (b) rough pasture'
+          ]
+        },
+        {
+          title: 'F Water and wetland',
+          collapsable: false,
+          children: [
+            'F1 Open water, coatal',
+            'F2, Open water, inland',
+            'F3 Wetland vegetation (a) peat bog',
+            'F3 Wetland vegetation (b) freshwater marsh',
+            'F3 Wetland vegetation (c) saltmarsh'
+          ]
+        },
+        {
+          title: 'G Rock and coastal land',
+          collapsable: false,
+          children: [
+            'G2 Bare rock (a) inland',
+            'G2 Bare rock (b) coastal',
+            'G3 Other coastal features (a) dunes',
+            'G3 Other coastal features (b) sand beach',
+            'G3 Other coastal features (c) shingle beach',
+            'G3 Other coastal features (d) mudflats'
+          ]
+        },
+        {
+          title: 'H Developed land',
+          collapsable: false,
+          children: [
+            'H1 Built-up land (a) urban area',
+            'H1 Built-up land (b) major transport route',
+            'H2 Quarries, mineral working and derelict land (a) quarries and mineral working',
+            'H2 Quarries, mineral working and derelict land (b) derelict land',
+            'H3 Isolated rural developments (a) farmsteads',
+            'H3 Isolated rural developments (b) other'
+          ]
+        },
+        {
+          title: 'I Unclassified land',
+          collapsable: false,
+          children: [
+            'I Unclassified land'
           ]
         }
       ],
